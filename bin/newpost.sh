509f36e4327b134c18a1551da3fb2845808fb321
schroeda <friedbiscuit@yf-19.net>
2021-05-19 07:08:26 +0900

    updated

--- bin/newpost.sh
+++ bin/newpost.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-basedir=/srv/github/godthumbs-cake/docs
+basedir=$(dirname $0)/../docs
 postdir=$basedir/_posts
 
 latestmdfile=$(ls -lrt $postdir |tail -1 |awk '{print $9;}')
 
-newmdfile=$postdir/$(date +%Y-%m-%d)-newposts.md
+newmdfile=$postdir/$(date +%Y-%m-%d)
 
+echo -n "title? ";read i
+newmdfile=$newmdfile-$(echo -n $i |sed -e 's/ /-/g').md
 echo "generating $newmdfile..."
 echo "relating $latestmdfile..."
 cp $postdir/$latestmdfile $newmdfile
