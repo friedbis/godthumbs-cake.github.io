2d0aedf640c02a8cbeb7e8783f3e42b6feeb8dfc
friedbis <friedbiscuit@yf-19.net>
2021-05-24 15:01:59 +0900

    newschecker updated

--- bin/newschecker.sh
+++ bin/newschecker.sh
@@ -16,7 +16,7 @@ xmlgetnext () {
 #fi
 
 basedir=$(dirname $0)/..
-rssurl="http://headline.5ch.net/bbynews/news.rss||http://vipsister23.com/index.rdf"
+rssurl="http://headline.5ch.net/bbynews/news.rss||http://vipsister23.com/index.rdf||https://news.yahoo.co.jp/rss/topics/top-picks.xml"
 templatefile=$(dirname $0)/post-template.txt
 postdate=$(date +%Y-%m-%d-%H-%M)
 #postcount=$(head -1 $countoffsetfile)
