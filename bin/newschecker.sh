6782167c68b6021298568d37f5072301b4a17cee
friedbis <friedbiscuit@yf-19.net>
2021-06-11 11:33:27 +0900

    cleaning

--- bin/newschecker.sh
+++ bin/newschecker.sh
@@ -16,7 +16,7 @@ xmlgetnext () {
 #fi
 
 basedir=$(dirname $0)/..
-rssurl="http://newmofu.doorblog.jp/rss/adult.xml||http://vipsister23.com/index.rdf||https://news.yahoo.co.jp/rss/topics/top-picks.xml||http://2ch-c.net/feed/?t=show"
+rssurl="https://www.ladyeve.net/feed||http://newmofu.doorblog.jp/rss/adult.xml||http://vipsister23.com/index.rdf||https://news.yahoo.co.jp/rss/topics/top-picks.xml||http://2ch-c.net/feed/?t=show"
 templatefile=$(dirname $0)/post-template.txt
 #postdate=$(date +%Y-%m-%d-%H-%M)
 postdate=$(date +%Y-%m-%d)
