6f1595f2bab79af2e6a2ee713c555490e1c386a8
friedbis <friedbiscuit@yf-19.net>
2021-08-18 20:54:59 +0900

    cleaning

--- bin/r15.sh
+++ bin/r15.sh
@@ -8,7 +8,13 @@ DSTDIR=$BASEDIR/docs/_posts
 #DSTDIR=/tmp
 nowdate=$(date +%Y-%m-%d)
 
-curl -s $R15MOVIEURL 2>&1 |tr -d '\r\n' |sed -e 's/^.*<tbody>\(.*\)\- \/main.*$/\1/; s/<td class="s"[^>]*>[\t 0-9]*<\/td>//g; s/<td class="sl"[^>]*>[^>]*<\/td>//g; s/\(>\)[\t ]*\(<\)/\1\2/g; s/<a href="\([^>]*\)">\([^>]*\)<\/a>/\2,https:\/\/www.necoweb.com\1\&/g; s/<[^>]*>//g; s/[\t ]\{2,20\}/\n/g;' |tr -d "\r\n" |sed -e 's/\([12][0-9]:[0-9]\{1,2\}\)/\n\1-/g; s/\([0-9]:[0-9]\{2\}\)\([^\-]\)/\n\1-\2/g; s/<!-$/\n/; s/\([0-9]\)\-/\1,/g; ' |grep -v "^$" |while read i;
+curl -s $R15MOVIEURL 2>&1 |\
+    tr -d '\r\n' |\
+    sed -e 's/^.*<tbody>\(.*\)\- \/main.*$/\1/; s/<td class="s"[^>]*>[\t 0-9]*<\/td>//g; s/<td class="sl"[^>]*>[^>]*<\/td>//g; s/\(>\)[\t ]*\(<\)/\1\2/g; s/<a href="\([^>]*\)">\([^>]*\)<\/a>/\2,https:\/\/www.necoweb.com\1\&/g; s/<[^>]*>//g; s/[\t ]\{2,20\}/\n/g;' |\
+    tr -d "\r\n" |\
+    sed -e 's/\([12][0-9]:[0-9]\{1,2\}\)/\n\1-/g; s/\([0-9]:[0-9]\{2\}\)\([^\-]\)/\n\1-\2/g; s/<!-$/\n/; s/\([0-9]\)\-/\1,/g; ' |\
+    grep -v "^$" |\
+    while read i;
 do
     #echo "[${i}]"
     posttitle=$(echo -n $i |awk 'BEGIN{FS=","} {print $2}')
