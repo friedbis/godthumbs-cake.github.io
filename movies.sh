3bbfc0936b20ce18e689828500e709832a991ffd
friedbis <friedbiscuit@yf-19.net>
2021-08-23 16:51:18 +0900

    updated

---
+++ movies.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $0)
+JSFILE=$BASEDIR/movies.js
+NODEBIN=$(which node)
+OUTPUTFILE=$BASEDIR/docs/movies.md
+
+if [ "x${NODEBIN}" == "x" ];
+then
+    echo "command was not found"
+    exit 1
+fi
+
+
+if [ ! -f $JSFILE ];
+then
+    echo "js file was not found"
+    exit 1
+fi
+
+
+$NODEBIN $JSFILE && git add $OUTPUTFILE && git commit -m 'movie updated' && git push -u origin main
+
+
