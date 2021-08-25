7bf008fa1a41ba5f3e366a11152a764cde9cc42c
friedbis <friedbiscuit@yf-19.net>
2021-08-25 20:23:15 +0900

    updated

--- bin/movies.sh
+++ bin/movies.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 BASEDIR=$(dirname $0)
+if [ -f $BASEDIR/../.profile ];
+then
+    . $BASEDIR/../.profile
+fi
+
 SYSNAME=$(basename $0)
 JSFILE=$BASEDIR/../movies.js
 NODEBIN=$(which node)
@@ -14,11 +19,6 @@ function _echo {
     echo $args
     logger -t $SYSNAME $args
 }
-
-if [ -f $BASEDIR/../.profile ];
-then
-    . $BASEDIR/../.profile
-fi
 
 if [ "x${NODEBIN}" == "x" ];
 then
@@ -36,7 +36,15 @@ fi
 cd $BASEDIR
 cd ..
 _echo "lets get the movie moderation script started..."
-$NODEBIN $JSFILE && git add $OUTPUTFILE && git commit -m 'movie updated' && git push -u origin main
+$NODEBIN $JSFILE
+if [ $? != 0 ];
+then
+    _echo "${JSFILE} was finished"
+    exit 1
+fi
+_echo "done"
+_echo "sync with git repository..."
+git add $OUTPUTFILE && git commit -m 'movie updated' && git push -u origin main
 _echo "done"
 
 
