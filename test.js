ccf52094ea19815cad7acfbe588a57bb12d8c393
friedbis <friedbiscuit@yf-19.net>
2021-08-28 19:00:16 +0900

    newschecker updated

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
-const kuro=require('kuroshiro');
+const kuroshiro=require('kuroshiro');
+const KuromojiAnalyzer=require("kuroshiro-analyzer-kuromoji");
 
 
+var test=async()=>{
+    await kuroshiro.init(KuromojiAnalyzer);
+    const result = await kuroshiro.convert("感じ取れたら手を繋ごう、重なるのは人生のライン and レミリア最高！", { to: "hiragana" });
 
+    console.log(result);
+}
+test();
