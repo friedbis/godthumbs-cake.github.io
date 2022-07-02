33063006c9a6c9fff79fbd5d172caeb17fced715
friedbis <friedbiscuit@yf-19.net>
2022-07-03 02:00:11 +0900

    newschecker updated

--- bin/mainte.sh
+++ bin/mainte.sh
@@ -4,5 +4,5 @@ BASEDIR=$(dirname $0)
 TARGETDIR=$BASEDIR/../docs/_posts
 
 echo "checking directory[${TARGETDIR}/]..."
-find $TARGETDIR/ -maxdepth 1 -size +5M -type f -print 
+find $TARGETDIR/ -maxdepth 1 -size +10M -type f -name 1999-01-08-archive.md -print 
 
