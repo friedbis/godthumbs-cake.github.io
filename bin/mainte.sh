a19525b4a21f14d07031e5a940d4ed153a075413
friedbis <friedbiscuit@yf-19.net>
2022-08-16 17:00:15 +0900

    newschecker updated

--- bin/mainte.sh
+++ bin/mainte.sh
@@ -3,10 +3,10 @@
 BASEDIR=$(dirname $0)
 TARGETDIR=$BASEDIR/../docs/_posts
 FILENAME=1999-01-08-archive.md
-FILESIZELIMIT=2 # file size until clean-up -> N[MB]
+FILESIZELIMIT=150 # file size until clean-up -> N[MB]
 
 echo "checking directory[${TARGETDIR}/]..."
-cat <(find $TARGETDIR/ -maxdepth 1 -size +${FILESIZELIMIT}M -type f -name ${FILENAME} -print) |while read i;
+cat <(find $TARGETDIR/ -maxdepth 1 -size +${FILESIZELIMIT}k -type f -name ${FILENAME} -print) |while read i;
 do
     echo "cleaning archive file..."
     mv $TARGETDIR/$FILENAME $TARGETDIR/$(date +%Y-%m-%d)-archive.md && echo "" >$TARGETDIR/$FILENAME
