a2f8c181ebbb19c7a45a0f0f1ae39dc3e96cb6bf
friedbis <friedbiscuit@yf-19.net>
2022-08-16 16:00:13 +0900

    newschecker updated

--- bin/mainte.sh
+++ bin/mainte.sh
@@ -3,7 +3,7 @@
 BASEDIR=$(dirname $0)
 TARGETDIR=$BASEDIR/../docs/_posts
 FILENAME=1999-01-08-archive.md
-FILESIZELIMIT=3 # file size until clean-up -> N[MB]
+FILESIZELIMIT=2 # file size until clean-up -> N[MB]
 
 echo "checking directory[${TARGETDIR}/]..."
 cat <(find $TARGETDIR/ -maxdepth 1 -size +${FILESIZELIMIT}M -type f -name ${FILENAME} -print) |while read i;
@@ -12,3 +12,4 @@ do
     mv $TARGETDIR/$FILENAME $TARGETDIR/$(date +%Y-%m-%d)-archive.md && echo "" >$TARGETDIR/$FILENAME
 done
 echo "OK"
+
